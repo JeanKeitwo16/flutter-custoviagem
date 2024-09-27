e81ceda32e1b5a8bfd575e59f048e772d60a194f
Jean <>
2024-09-26 22:45:17 -0300

    segundo commit

---
+++ lib/carro.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Carro extends StatelessWidget {
+  const Carro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
